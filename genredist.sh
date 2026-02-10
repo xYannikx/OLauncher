43791e50f4b7a4e2d2ea56879184cda57672f242
RagedMeteor1837 <148640131+RagedMeteor1837@users.noreply.github.com>
2026-02-10 18:42:48 +0000

    2.2.1

--- genredist.sh
+++ genredist.sh
@@ -4,7 +4,7 @@ basedir=$(pwd)
 autooldir=$basedir/AutoOL
 workdir=$basedir/work
 
-OLAUNCHER_VERSION=2.2.0
+OLAUNCHER_VERSION=2.2.1
 AUTOOL_VERSION=0.1.0
 
 finalname="olauncher-$OLAUNCHER_VERSION-redist.jar"
